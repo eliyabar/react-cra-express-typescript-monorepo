1157748b2ad410ee8da864d3787ab8e2aa2e3a85
Eliya BarOn <flashxyz@gmail.com>
2020-06-04 12:01:06 +0300

    increase port

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,6 +1,6 @@
 import server from './server'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 server().listen(port, () =>
     console.log(`App is listening on localhost:${port}`)
 )
